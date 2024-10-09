0872f1b1746d0974b636e90005368fa2b970a1c2
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2024-10-09 16:28:37 +0530

    update UI

--- frontend/src/app/components/Thumplike/page.js
+++ frontend/src/app/components/Thumplike/page.js
@@ -1,21 +1,27 @@
+'use client';
+import React from 'react';
+import { FaThumbsDown, FaThumbsUp } from 'react-icons/fa';
+import moment from 'moment';
 
-'use client'
-import React from 'react'
-import { FaThumbsDown, FaThumbsUp } from 'react-icons/fa'
+const Thumplike = ({ date, like, dislike, id }) => {
 
-const Thumplike = () => {
+    const days = moment().diff(moment(date), 'days');
+
     return (
         <div className="absolute right-0 bottom-0 flex items-center p-4 space-x-4">
-
             <button className="text-gray-800 dark:text-gray-200 cursor-pointer">
-                <FaThumbsUp />
+                <div className="tooltip tooltip-top z-50" data-tip={like}>
+                    <FaThumbsUp />
+                </div>
             </button>
             <button className="text-gray-800 dark:text-gray-200 cursor-pointer">
-                <FaThumbsDown />
+                <div className="tooltip tooltip-top z-50" data-tip={dislike}>
+                    <FaThumbsDown />
+                </div>
             </button>
-            <span className="text-white">3 days ago</span>
+            <span className="text-white">{days} days ago</span>
         </div>
-    )
+    );
 }
 
-export default Thumplike
+export default Thumplike;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -13,5 +13,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailgrids/plugin")],
+  plugins: [require("tailgrids/plugin"), require('daisyui')],
 };
